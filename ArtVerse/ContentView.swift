169db0c8548405f80649c322f74faa8c9c81b8b1
Ahmed Henna <85130496+AhmedHenna@users.noreply.github.com>
2023-05-31 00:42:01 +0300

    Started implementation on home screen

--- ArtVerse/ContentView.swift
+++ ArtVerse/ContentView.swift
@@ -9,18 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(alignment: .leading, spacing: 8.0) {
+            Spacer()
+            Image("Procreate")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 26.0, height: 26.0)
+                .cornerRadius(10)
+                .padding(9)
+                .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 20, style: .continuous))
+            Text("Procreate for Beginners")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundStyle(.linearGradient(colors: [.primary, .primary.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
+            Text("5 sections - 4 hours".uppercased())
+                .font(.footnote)
+                .fontWeight(.semibold)
+                .foregroundStyle(.secondary)
+            Text("Learn all the basic knowledge about Procreate and from brushes, colors...")
+                .font(.footnote)
+                .lineLimit(2)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .foregroundColor(.secondary)
         }
-        .padding()
+        .padding(.all, 20.0)
+        .padding(.vertical, 20)
+        .frame(height: 350.0)
+        .background(.ultraThinMaterial)
+        .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
+        .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+        .overlay(
+            RoundedRectangle(cornerRadius: 30, style: .continuous)
+                .stroke(.linearGradient(colors: [.white.opacity(0.4),.black.opacity(0.1)], startPoint: .top, endPoint: .bottom))
+                .blendMode(.overlay)
+        )
+        .padding(.horizontal, 20)
+        .background(Image("Blob").offset(x:250, y:-100))
+        .overlay(Image("Illustration 3")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height:141)
+                .offset(x:0, y:-70))
+        
+
+        
+        
+    
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        ContentView()
         ContentView()
     }
 }
