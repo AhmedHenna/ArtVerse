38328622a3476161a7803d02624ee2dc4119a338
Ahmed Henna <85130496+AhmedHenna@users.noreply.github.com>
2023-05-30 12:20:49 +0300

    Initial Commit

---
+++ ArtVerse/ArtVerseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtVerseApp.swift
+//  ArtVerse
+//
+//  Created by Ahmed Henna on 5/30/23.
+//
+
+import SwiftUI
+
+@main
+struct ArtVerseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
