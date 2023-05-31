2ee80214f0c552cc22f87fa8ad4ed50966ae84d7
Ahmed Henna <85130496+AhmedHenna@users.noreply.github.com>
2023-05-31 15:15:08 +0300

    updated account page

--- ArtVerse/AccountView.swift
+++ ArtVerse/AccountView.swift
@@ -8,49 +8,105 @@
 import SwiftUI
 
 struct AccountView: View {
+    @State var isDeleted = false
+    @State var isPinned = false
+    
     var body: some View {
         NavigationView {
             List{
-                VStack {
-                    Image(systemName: "person.crop.circle.fill.badge.checkmark")
-                        .font(.system(size: 32))
-                        .symbolRenderingMode(.palette)
-                        .foregroundStyle(.blue, .blue.opacity(0.3))
-                        .padding()
-                        .background(Circle().fill(.ultraThinMaterial))
-                        .background(
-                            Image(systemName: "hexagon")
-                                .symbolVariant(.fill)
-                                .foregroundColor(.blue)
-                                .font(.system(size:200))
-                                .offset(x: -45, y: -120)
-                    )
-                    Text("Ahmed Henna")
-                        .font(.title.weight(.semibold))
-                    HStack{
-                        Image(systemName: "location").imageScale(.large)
-                        Text("Egypt").foregroundColor(.secondary)
-                    }
-                }
-                .frame(maxWidth: .infinity)
-                .padding()
-                
-            
-                Section {
-                    Text("Ahmed")
-                    Text("Billing")
-                    Text("Help")
-                }
-               
+                profile
+                menu
+                links
             }
             .listStyle(.insetGrouped)
             .navigationTitle("Account")
         }
     }
+    
+    var profile: some View{
+        VStack (spacing: 8) {
+            Image(systemName: "person.crop.circle.fill.badge.checkmark")
+                .font(.system(size: 32))
+                .symbolRenderingMode(.palette)
+                .foregroundStyle(.blue, .blue.opacity(0.3))
+                .padding()
+                .background(Circle().fill(.ultraThinMaterial))
+                .background(
+                    Image(systemName: "hexagon")
+                        .symbolVariant(.fill)
+                        .foregroundColor(.blue)
+                        .font(.system(size:200))
+                        .offset(x: -45, y: -120)
+                )
+            Text("Ahmed Henna")
+                .font(.title.weight(.semibold))
+            HStack{
+                Image(systemName: "location").imageScale(.medium)
+                Text("Egypt").foregroundColor(.secondary)
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .padding()
+        
+    }
+    
+    var menu : some View{
+        Section {
+            NavigationLink {ContentView()} label: {
+                Label("Settings", systemImage: "gear")
+            }
+            NavigationLink {ContentView()} label: {
+                Label("Billing", systemImage: "creditcard")
+            }
+            NavigationLink {ContentView()} label: {
+                Label("Help", systemImage: "questionmark")
+            }
+        }
+        .foregroundColor(.primary)
+        .listRowSeparator(.hidden)
+    }
+    
+    var links: some View{
+        Section {
+            if !isDeleted {
+                Link(destination: URL(string: "http://youssefhenna.com")!) {
+                    HStack {
+                        Label("Website", systemImage: "house")
+                        Spacer()
+                        Image(systemName: "link")
+                            .foregroundColor(.secondary)
+                    }
+                }
+                .swipeActions(edge: .leading, allowsFullSwipe: false){
+                    deleteButton
+                    pinButton
+                }
+            }
+        }
+        .foregroundColor(.primary)
+        .listRowSeparator(.hidden)
+    }
+    
+    var deleteButton: some View{
+        Button(action: {isDeleted = true}) {
+            Label("Delete", systemImage: "trash")
+        }
+        .tint(.red)
+    }
+    
+    var pinButton: some View{
+        Button(action: {isPinned.toggle()}){
+            Label("Pin", systemImage: !isPinned ? "pin" : "pin.slash")
+        }
+        .tint(!isPinned ? .yellow: .gray)
+    }
+
 }
 
 struct AccountView_Previews: PreviewProvider {
     static var previews: some View {
         AccountView()
+        AccountView()
+            .preferredColorScheme(.dark)
     }
 }
