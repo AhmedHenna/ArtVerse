580ea399ae57f5d411ef590e19c5304c7615a00a
Ahmed Henna <85130496+AhmedHenna@users.noreply.github.com>
2023-06-01 22:32:12 +0300

    implemented custom tab bar

---
+++ ArtVerse/TabBar.swift
@@ -0,0 +1,138 @@
+//
+//  TabBar.swift
+//  ArtVerse
+//
+//  Created by Ahmed Henna on 6/1/23.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    @State var selectedTab: Tab = .home
+    @State var color: Color = .teal
+    @State var tabItemWidth: CGFloat = 0
+    
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            
+            Group {
+                switch selectedTab {
+                case .home:
+                    ContentView()
+                case .explore:
+                    AccountView()
+                case .notifications:
+                    AccountView()
+                case .library:
+                    AccountView()
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            
+            HStack {
+                buttons
+            }
+            .padding(.horizontal, 10)
+            .padding(.top, 15)
+            .frame(height: 90, alignment: .top)
+            .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 34, style: .continuous))
+            .background(
+                circleGlow
+            )
+            .overlay(
+                lineIndicator
+            )
+            .strokeStyle(cornerRadius: 34)
+            .frame(maxHeight: .infinity, alignment: .bottom)
+            .ignoresSafeArea()
+        }
+    }
+    
+    var buttons: some View {
+        ForEach(tabItems) { item in
+            Button {
+                withAnimation(.spring(response: 0.3, dampingFraction: 0.8)) {
+                    selectedTab = item.tab
+                    color = item.color
+                }
+            } label: {
+                VStack(spacing: 0) {
+                    Image(systemName: item.icon)
+                        .symbolVariant(.fill)
+                        .font(.body.bold())
+                        .frame(width: 50, height: 29)
+                    Text(item.text)
+                        .font(.caption2)
+                }
+                .frame(maxWidth: .infinity)
+            }
+            .foregroundStyle(selectedTab == item.tab ? .primary : .secondary)
+            .blendMode(selectedTab == item.tab ? .overlay : .normal)
+            .overlay(
+                //geometry reader is used to make sure circle and line fit properly on all screens
+                GeometryReader { proxy in
+                    Color.clear.preference(key: TabPreferenceKey.self, value: proxy.size.width)
+                }
+            )
+            .onPreferenceChange(TabPreferenceKey.self) { value in
+                tabItemWidth = value
+            }
+        }
+    }
+    
+    //this method is for moving the circle glow behind each icon
+    var circleGlow: some View {
+        HStack {
+            //left spacers
+            if selectedTab == .library { Spacer() }
+            if selectedTab == .explore { Spacer() }
+            if selectedTab == .notifications {
+                Spacer()
+                Spacer()
+            }
+            Circle().fill(color).frame(width: tabItemWidth)
+            //right spacers
+            if selectedTab == .home { Spacer() }
+            if selectedTab == .explore {
+                Spacer()
+                Spacer()
+            }
+            if selectedTab == .notifications { Spacer() }
+        }
+        .padding(.horizontal, 8)
+    }
+    
+    //this method is to move the line above each icon
+    var lineIndicator: some View {
+        HStack {
+            //left spacers
+            if selectedTab == .library { Spacer() }
+            if selectedTab == .explore { Spacer() }
+            if selectedTab == .notifications {
+                Spacer()
+                Spacer()
+            }
+            Rectangle()
+                .fill(color)
+                .frame(width: 28, height: 5)
+                .cornerRadius(3)
+                .frame(width: tabItemWidth)
+                .frame(maxHeight: .infinity, alignment: .top)
+            //right spacers
+            if selectedTab == .home { Spacer() }
+            if selectedTab == .explore {
+                Spacer()
+                Spacer()
+            }
+            if selectedTab == .notifications { Spacer() }
+        }
+        .padding(.horizontal, 8)
+    }
+}
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBar()
+.previewInterfaceOrientation(.portrait)
+    }
+}
