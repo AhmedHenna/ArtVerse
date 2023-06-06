6359ffe8dc019e3478681abe1c618d7e2a210579
Ahmed Henna <85130496+AhmedHenna@users.noreply.github.com>
2023-06-06 23:09:31 +0300

    Added courses data file + cleaned up code + made custom swiper on home screen

--- ArtVerse/Components/FeaturedItem.swift
+++ ArtVerse/Components/FeaturedItem.swift
@@ -8,10 +8,12 @@
 import SwiftUI
 
 struct FeaturedItem: View {
+    var course: Course = courses[0]
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 8.0) {
             Spacer()
-            Image("Procreate")
+            Image(course.logo)
                 .resizable(resizingMode: .stretch)
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 26.0, height: 26.0)
@@ -19,15 +21,15 @@ struct FeaturedItem: View {
                 .padding(9)
                 .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 16, style: .continuous))
                 .strokeStyle(cornerRadius: 16)
-            Text("Procreate for Beginners")
+            Text(course.title)
                 .font(.title)
                 .fontWeight(.bold)
                 .foregroundStyle(.linearGradient(colors: [.primary, .primary.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
-            Text("5 sections - 4 hours".uppercased())
+            Text(course.subtitle.uppercased())
                 .font(.footnote)
                 .fontWeight(.semibold)
                 .foregroundStyle(.secondary)
-            Text("Learn all the basic knowledge about Procreate and from brushes, colors...")
+            Text(course.text)
                 .font(.footnote)
                 .lineLimit(2)
                 .frame(maxWidth: .infinity, alignment: .leading)
@@ -35,18 +37,17 @@ struct FeaturedItem: View {
         }
         .padding(.all, 20.0)
         .padding(.vertical, 20)
-        .frame(height: 350.0)
+        .frame(height: 380.0)
         .background(.ultraThinMaterial)
         .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
         .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
         .strokeStyle()
         .padding(.horizontal, 20)
-        .background(Image("Blob").offset(x:250, y:-100))
-        .overlay(Image("Illustration 3")
+        .overlay(Image(course.image)
             .resizable()
             .aspectRatio(contentMode: .fit)
-            .frame(height:141)
-        .offset(x:0, y:-80))
+            .frame(height:course.height)
+            .offset(x:0, y:-100))
     }
 }
 
--- ArtVerse/Views/HomeView.swift
+++ ArtVerse/Views/HomeView.swift
@@ -11,30 +11,46 @@ struct HomeView: View {
     @State var hasScrolled = false
     
     var body: some View {
-        ScrollView {
-            GeometryReader{ proxy in
-                Color.clear.preference(key: ScrollPreferenceKey.self, value: proxy.frame(in: .named("scroll")).minY)
-            }.frame(height: 0)
-            
-            FeaturedItem()
-            Color.clear.frame(height:500)
-        }
-        .coordinateSpace(name: "scroll")
-        .onPreferenceChange(ScrollPreferenceKey.self, perform: {value in
-            withAnimation (.easeInOut){
-                if value < 0{
-                    hasScrolled = true
-                }else{
-                    hasScrolled = false
-                }
+        ZStack {
+            Color("Background").ignoresSafeArea()
+            ScrollView {
+                detectScroll
+                coursesFeaturedSwiper
+                Color.clear.frame(height:500)
             }
-        })
-        .safeAreaInset(edge: .top, content: {
-            Color.clear.frame(height: 70)
-        })
-        .overlay(AppBar(title: "Featured", hasScrolled: $hasScrolled))
+            .coordinateSpace(name: "scroll")
+            .onPreferenceChange(ScrollPreferenceKey.self, perform: {value in
+                withAnimation (.easeInOut){
+                    if value < 0{
+                        hasScrolled = true
+                    }else{
+                        hasScrolled = false
+                    }
+                }
+            })
+            .safeAreaInset(edge: .top, content: {
+                Color.clear.frame(height: 70)
+            })
+            .overlay(AppBar(title: "Featured", hasScrolled: $hasScrolled))
+        }
     }
     
+    var detectScroll: some View{
+        GeometryReader{ proxy in
+            Color.clear.preference(key: ScrollPreferenceKey.self, value: proxy.frame(in: .named("scroll")).minY)
+        }.frame(height: 0)
+    }
+    
+    var coursesFeaturedSwiper: some View{
+        TabView {
+            ForEach(courses) { item in
+                FeaturedItem(course: item)
+            }
+        }
+        .tabViewStyle(.page(indexDisplayMode: .never))
+        .frame(height: 430)
+        .background(Image("Blob").offset(x:250, y:-100))
+    }
 }
 
 struct HomeView_Previews: PreviewProvider {
